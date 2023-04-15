565fba2113180b176cebadeafb591a8b48ddeccb
Beenarob <beenarobert16@gmail.com>
2023-04-15 16:14:49 -0700

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => {
+  console.log('MongoDB connection successful.');
+}).catch((e) => {
+  console.log('MongoDB connection error: ', e.message)
 });
 
 module.exports = mongoose.connection;
